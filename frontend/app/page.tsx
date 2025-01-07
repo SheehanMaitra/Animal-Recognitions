094896a13221403f3432a67bbbd909eb5d25d9e2
Sheehan <maitrasheehan@gmail.com>
2025-01-07 00:56:31 -0500

    Next.js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+import LandingPage from '../components/LandingPage';
+import Finder from '../components/Finder';
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    <LandingPage />
+    <Finder />
+    </>
+  );
+}
